f55aed6f7b1fb320eb277ba423b78a0475bd2e24
TAL <tenenberg@gmail.com>
2022-03-27 21:07:08 +0300

    nx demo

--- apps/t1/src/app/app.module.ts
+++ apps/t1/src/app/app.module.ts
@@ -3,11 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromProducts from './apps/t1/products/products.reducer';
+import { ProductsEffects } from './apps/t1/products/products.effects';
+import { ProductsFacade } from './apps/t1/products/products.facade';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    StoreModule.forFeature(
+      fromProducts.PRODUCTS_FEATURE_KEY,
+      fromProducts.reducer
+    ),
+    EffectsModule.forFeature([ProductsEffects]),
+  ],
+  providers: [ProductsFacade],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
